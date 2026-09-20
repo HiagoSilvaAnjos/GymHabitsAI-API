23af909d4e7ab43b4c13f57d84f74c0e59efaf03
HiagoSilva <hiagosilvaanjos7@gmail.com>
2026-09-20 17:56:38 -0300

    chore: add better-auth setup

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import {
 import z from "zod";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import { auth } from "./lib/auth.js";
+import { fromNodeHeaders } from "better-auth/node";
+import fastifyCors from "@fastify/cors";
 
 const app = Fastify({
   logger: true,
@@ -35,6 +38,11 @@ await app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
 });
 
+await app.register(fastifyCors, {
+  origin: "localhost:3000",
+  credentials: true,
+});
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
@@ -51,6 +59,32 @@ app.withTypeProvider<ZodTypeProvider>().route({
     return {
       message: "Server is running",
     };
+  },
+});
+
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      const url = new URL(request.url, `http://${request.headers.host}`);
+      const headers = fromNodeHeaders(request.headers);
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+      const response = await auth.handler(req);
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      return reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error);
+      return reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
+    }
   },
 });
 
